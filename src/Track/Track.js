edb6ea1552822617d761bdcc84642ea80f2c5df4
Sidney Russell <sidneyrussell58@gmail.com>
2025-10-25 13:33:05 -0600

    Updated for add and remove buttons

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,34 +1,35 @@
 import React, { useCallback } from 'react';
+import "./Track.module.css";
 
-const Track = (props) => {
-    const addTrack = useCallback(
-        (event) => {
+function Track(props) {
+    const addTrack = useCallback(() => {
             props.onAdd(props.track);
-        },
-        [props.onAdd, props.track]
-    );
+        }, [props]);
 //this function should trigger when the user presses the "add"
 //button (add to css)
-    const removeTrack = useCallback(
-        (event) => {
+
+    const removeTrack = useCallback(() => {
             props.onRemove(props.track);
-        },
-        [props.onRemove, props.track]
-    );
+        }, [props]);
 //this function should trigger when the user presses the "remove"
 //button (add to css)
-    const renderAction = () => {};
+
+    const renderAction = () => {
+        if (props.isRemoval) {
+            return <button className="Track-action" onClick={removeTrack}>-</button>;
+        }
+        return <button className="Track-action" onClick={addTrack}>+</button>;
+    };
 
     return (
-        <div>
-            <p id='name'>Song Name:</p>
-            <input/>
-            <p id='artist'>Artist:</p>
-            <input/>
-            <p id='album'>Album:</p>
-            <input/>
+        <div className="Track">
+            <div className="Track-information">
+                <h3>{props.track.name}</h3>
+                <p>{props.track.artist} | {props.track.album}</p>
+            </div>
+            {renderAction()}
         </div>
     );
-};
+}
 
 export default Track;

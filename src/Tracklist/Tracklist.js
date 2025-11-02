7e8e3a4f61c4ddbcd324c8c05e5a87e64510f389
Sidney Russell <sidneyrussell58@gmail.com>
2025-11-02 13:24:52 -0700

    Added a fallback to handle empty lists

--- src/Tracklist/Tracklist.js
+++ src/Tracklist/Tracklist.js
@@ -2,18 +2,22 @@ import React from 'react';
 import "./Tracklist.css";
 import Track from "../Track/Track";
 
-function Tracklist(props) {
+function Tracklist({ tracks = [], onAdd, onRemove, isRemoval }) {
   return (
     <div className="Tracklist">
-      {props.tracks.map((track) => (
-        <Track 
-          key={track.id}
-          track={track}
-          onAdd={props.onAdd}
-          onRemove={props.onRemove}
-          isRemoval={props.isRemoval}
-        />
-      ))}
+      {tracks.length > 0 ? (
+        tracks.map((track) => (
+          <Track 
+            key={track.id}
+            track={track}
+            onAdd={props.onAdd}
+            onRemove={props.onRemove}
+            isRemoval={props.isRemoval}
+          />
+        ))
+      ) : (
+        <p className='no-tracks'>No tracks to display.</p>
+      )}
     </div>
   );
 }

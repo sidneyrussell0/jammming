ba57fd255a682d9f28ab96872ed8baf3c0ade4da
Sidney Russell <sidneyrussell58@gmail.com>
2025-11-02 13:28:01 -0700

    Removed props on addTrack and removeTrack to narrow dependencies to the specific props

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -3,14 +3,14 @@ import "./Track.css";
 
 function Track(props) {
     const addTrack = useCallback(() => {
-            props.onAdd(props.track);
-        }, [props]);
+            onAdd(track);
+        }, [onAdd, track]);
 //this function should trigger when the user presses the "add"
 //button (add to css)
 
     const removeTrack = useCallback(() => {
-            props.onRemove(props.track);
-        }, [props]);
+            onRemove(track);
+        }, [onRemove, track]);
 //this function should trigger when the user presses the "remove"
 //button (add to css)
 

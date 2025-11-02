4020d03848b61c2184f4993816a5bd452f6d48ce
Sidney Russell <sidneyrussell58@gmail.com>
2025-11-02 12:21:04 -0700

    Made clientId safe for production

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,5 @@
-const clientId = 'd21d75df48fa44789360a68f8ff275b6'; // Insert client ID here.
-const redirectUri = 'https://violety-hee-inaptly.ngrok-free.dev/'; // Have to add this to your accepted Spotify redirect URIs on the Spotify API.
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID || 'd21d75df48fa44789360a68f8ff275b6';
+const redirectUri = 'https://violety-hee-inaptly.ngrok-free.dev/'; 
 let accessToken;
 
 const Spotify = {
